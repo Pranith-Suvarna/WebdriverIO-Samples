cbe1b6f99d8aa7d52d6409c023a2f9a1b300674c
pranith <pranithsuvarna1@gmail.com>
2022-04-14 03:23:38 +0530

    Webdriver IO C1

---
+++ basics.js
@@ -0,0 +1,32 @@
+
+//for loop
+for(let k=1;k<=10;k++)
+{
+if(k%2==0)
+{
+  console.log(k)  
+}
+}
+
+//if-else
+const flag = true
+
+if (!flag) {
+    console.log("Condition satisfied")
+} else {
+    console.log(flag)
+    console.log("Condition not satisfied")
+}
+
+//while loop
+let i=0
+while(i<10){
+i++
+console.log(i)
+}
+
+//do-while loop
+do{
+    i++
+}while(i>10);
+console.log(i)
